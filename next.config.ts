71698095d9ebb381e617aa6affa890ee39a16ae8
Gia Huy <121534749+baesuzy02@users.noreply.github.com>
2025-05-26 17:59:01 +0700

    feat: test

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://server-wv4w.onrender.com/api/:path*',
       },
     ];
   },
